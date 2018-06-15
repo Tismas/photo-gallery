b51ba6b822d0c8dc1044d5288ad2d6cef19d2762
Nasos <adamski256@gmail.com>
2018-06-15 11:59:33 +0200

    project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../styles/app.scss';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello world
+            </div>
+        );
+    }
+}
+
+export default App;
